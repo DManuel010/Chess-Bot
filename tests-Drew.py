ec0b25687543434f2c9f4554e285be01b17f8f2e
Pascal <pascalsbakker@gmail.com>
2017-03-22 18:27:40 -0400

    Minimax
    I created a prototype minimax function

--- tests-Drew.py
+++ tests-Drew.py
@@ -19,10 +19,17 @@ def pascalTest():
     board = chess.Board()
     color = True #True=White Black=false
     num = 0 #Number of moves
-
+    turn = True
     #Runs the chess game until win, draw, or stalemate hmmm
     while(True):
-        board.push(randomAI(board))
+        if turn:
+            board.push(pascalMinimax(board,2))
+            color = False
+            turn = False
+        else:
+            board.push(randomAI(board))
+            color = True
+            turn = True
         num=num+1
         print board
         print "=============================="
@@ -62,7 +69,74 @@ def drewTest():
             stillPlaying = False
             print "number of moves: " , turns
 
+<<<<<<< Updated upstream
         elif board.is_game_over() and board.is_checkmate():
+=======
+        elif board.is_game_over() or board.is_checkmate():
+            print "DRAW"
+            stillPlaying = false
+            print "number of moves: " , turns
+def pascalMinimax(board,depth):
+    best_move = None
+    best_score = -9999
+    newBoard = board #Makes the move on the board copy
+    moves = []
+    #Creates a list of moves from legal moves
+    for move in board.legal_moves:
+        moves.append(move)
+    #Executes minimax function
+    for move in moves:
+        newBoard.push(move)
+        score = mini(newBoard, depth)
+        if score > best_score:
+            best_move = move
+            best_score = score
+        newBoard.pop() #undos last move
+    return chess.Move.from_uci(str(best_move))
+
+def maxi(gameState, depth):
+        if gameState.is_game_over(): return evaluate(gameState)
+        if depth==0: return 0
+        maxScore = -99999
+        newBoard = gameState
+        #Generates a list of moves from legal moves
+        moves = []
+        for move in gameState.legal_moves:
+            moves.append(move)
+        #Executes max function
+        for move in moves:
+            newBoard.push(move)
+            score = mini(newBoard, depth-1) #Calls min function
+            if score > maxScore:
+                maxScore = score
+            newBoard.pop() #undos last move
+        return maxScore
+def mini(gameState, depth):
+        if gameState.is_game_over(): return evaluate(gameState)
+        if depth ==0: return 0
+        minScore = 99999
+        newBoard = gameState
+        #Generates a list of moves from legal moves
+        moves = []
+        for move in gameState.legal_moves:
+            moves.append(move)
+        #Executes max function
+        for move in moves:
+            newBoard.push(move)
+            score = maxi(newBoard, depth-1) #Calls min function
+            if score < minScore:
+                minScore = score
+            newBoard.pop() #undos last move
+        return minScore
+
+    #Evaluates terminal state
+def evaluate(board):
+        if (board.is_stalemate() or board.is_insufficient_material() or
+            board.can_claim_threefold_repetition() or board.can_claim_fifty_moves() or
+            board.can_claim_draw()):
+            return 0
+        else: return 10
+>>>>>>> Stashed changes
 
             if player_white:
                 print "White Won"
