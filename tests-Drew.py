45ae3c33a7d13826ba0be426ba57acf59b7950a4
Pascal <pascalsbakker@gmail.com>
2017-03-23 10:48:00 -0400

    Added Prototype stockfish AI

--- tests-Drew.py
+++ tests-Drew.py
@@ -1,22 +1,14 @@
 import chess
-from minimax import pascalMinimax
-from alpha_beta import alpha_beta_search
-from random_AI import randomAI
 import random
-
+from minimax import pascalMinimax
+from minimax import alpha_beta_search
 
 def startGame():
-
     # clear_stack
     return chess.Board()
 
 
 
-def main():
-    '''
-    Simulates a game of chess by looping two AI players' actions
-    '''
-    drewTest()
 def randomAI(board): #Selects one of the legal moves randomly and returns it
     moves = []
     for move in board.legal_moves:
@@ -27,47 +19,45 @@ def randomAI(board): #Selects one of the legal moves randomly and returns it
 
     return chess.Move.from_uci(str(move))
 
-def stockfishAI(board):
-    import chess.uci
-    engine = chess.uci.popen_engine("stockfish")
-    engine.uci()
-    print engine.name
-    engine.position(board)
-    bestmove, ponder = engine.go(movetime=2000)
-    return chess.Move.from_uci(str(bestmove))
+
 
 def drewTest():
     turns = 0               # turn counter
     stillPlaying = True     # outer loop boolean
     player1 = "white"
     player2 = "black"
-
     players = [player1, player2]
-    board = startGame()             # gets initial board state
+    board = startGame()
     print board
-
     while stillPlaying:
-
         turns += 1
         if turns % 2 > 0:
             currentPlayer = players[0]
-            board.push(pascalMinimax(board,2,currentPlayer))
+            board.push(pascalMinimax(board,1,currentPlayer))
             #move = alpha_beta_search(board, currentPlayer,2) DO NOT DO ALPHA BETA IT DOES NOT WORK
+            #board.push(move)
         else:
             currentPlayer = players[1]
-            board.push(randomAI(board))
+            board.push(stockfishAI(board))
+
 
         print board
         print "=================="
 
         if (board.is_stalemate() or board.is_insufficient_material()):
+
             print "DRAW"
             print "number of moves: " , turns
             stillPlaying = False
 
         elif board.is_game_over() and board.is_checkmate():
+
             print "Winner: " , currentPlayer
             print "number of moves: " , turns
             stillPlaying = False
+
+def main():
+
+    drewTest()
 
 main()
