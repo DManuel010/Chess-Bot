501b1cb5b6de6669f4021b5ed665a897761b5200
Pascal <pascalsbakker@gmail.com>
2017-03-23 00:07:44 -0400

    Created a working greedy hueristic
    I created a greedy heuristic in pascalH.py
    
    I also tried implementing an alpha- pruning method but it currently
    does not work. It is in the minimax.py

--- minimax.py
+++ minimax.py
@@ -1,5 +1,6 @@
 import chess
 import random
+from pascalH import pascalHueristic
 
 def pascalMinimax(board,depth,player):
     best_move = None
@@ -17,14 +18,13 @@ def pascalMinimax(board,depth,player):
             best_move = move
             best_score = score
         newBoard.pop() #undos last move
-    print best_score
     return chess.Move.from_uci(str(best_move))
 
 
 
 def maxi(gameState, depth, player):
         if gameState.is_game_over(): return evaluate(gameState, "black")
-        if depth==0: return 0
+        if depth==0: return pascalHueristic(gameState)
         maxScore = -99
         newBoard = gameState
         #Generates a list of moves from legal moves
@@ -46,7 +46,7 @@ def mini(gameState, depth, player):
         if gameState.is_game_over():
             return evaluate(gameState, "white")
         if depth == 0:
-            return 0
+            return pascalHueristic(gameState)
         minScore = 99
         newBoard = gameState
         #Generates a list of moves from legal moves
@@ -77,3 +77,76 @@ def evaluate(board, player):
 
         else:
             return -999
+
+
+def alpha_beta_search(board, player,depth):
+        best_move = None
+        infinity = float('inf')
+        best_val = -infinity
+        beta = infinity
+        newBoard = board #Makes the move on the board copy
+        moves = []
+        #Creates a list of moves from legal moves
+        for move in board.legal_moves:
+            moves.append(move)
+
+        for move in moves:
+            newBoard.push(move)
+            print newBoard.legal_moves
+            value = min_value(newBoard, best_val, beta,player,depth)
+            if value > best_val:
+                best_val = value
+                best_move = move
+            newBoard.pop()
+
+        return chess.Move.from_uci(str(best_move))
+
+def max_value(gameState, alpha, beta, player,depth):
+        if gameState.is_game_over():
+            getUtility(gameState,player)
+        if depth==0:
+            return pascalHueristic(gameState)
+        infinity = float('inf')
+        value = -infinity
+        newState=gameState
+        successors= []
+        for move in gameState.legal_moves:
+            successors.append(move)
+        if len(successors)==0:
+            return pascalHueristic(gameState)
+        for state in successors:
+            newState.push(state)
+            newState.pop()
+            value = max(value, min_value(newState, alpha, beta,player,depth-1))
+            if value >= beta:
+                return value
+            alpha = max(alpha, value)
+            newState.pop()
+        return value
+
+def min_value(gameState, alpha, beta, player,depth):
+        if gameState.is_game_over():
+            getUtility(gameState,player)
+        if depth==0:
+            return pascalHueristic(gameState)
+        infinity = float('inf')
+        value = infinity
+        newState=gameState
+        successors= []
+        for move in gameState.legal_moves:
+            successors.append(move)
+        if len(successors)==0:
+            return pascalHueristic(gameState)
+        for state in successors:
+            newState.push(state)
+            value = min(value, max_value(newState, alpha, beta,player,depth-1))
+            if value <= alpha:
+                return value
+            beta = min(beta, value)
+
+            newState.pop()
+        return value
+
+def getUtility(gameState,player):
+        assert gameState is not None
+        return evaluate(gameState,player)
